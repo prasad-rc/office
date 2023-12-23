5510a0672baffd9af3044982e88b2a3654c3fd00
DURGA PRASAD R C <87293361+prasad-rc@users.noreply.github.com>
2023-12-23 22:38:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function showForm(formId) {
+    document.getElementById('form1').style.display = 'none';
+    document.getElementById('form2').style.display = 'none';
+
+    document.getElementById(formId).style.display = 'block';
+}
